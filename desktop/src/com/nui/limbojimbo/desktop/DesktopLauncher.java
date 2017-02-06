f6296f64a278b6546a04c82586d07be104b18d6b
ayushigarg1992 <garg.bulbul@gmail.com>
2017-02-05 20:11:54 -0500

    project init

---
+++ desktop/src/com/nui/limbojimbo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nui.limbojimbo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nui.limbojimbo.LimboJimbo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LimboJimbo(), config);
+	}
+}
